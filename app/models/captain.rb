06397cc0243b81647625e7e1418ec01432110b9f
Esmery Corniel <esmerycornielle@gmail.com>
2016-10-31 14:06:47 -0400

    Done

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    joins(:classifications).where("classifications.name = 'Catamaran'").uniq
+  end
+
+  def self.sailors
+    joins(:classifications).where("classifications.name = 'Sailboat'").uniq
+  end
+
+  def self.motorboarders
+    joins(:classifications).where("classifications.name = 'Motorboat'").uniq
+  end
+
+  def self.talented_seamen
+    sailors.where(id: motorboarders.ids)
+  end
+
+  def self.non_sailors
+    where.not(id: sailors.ids)
+  end
+
 end
